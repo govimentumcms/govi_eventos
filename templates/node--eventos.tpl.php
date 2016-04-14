f7c75aa55baffe76b81dbb6a9899632754a8bbdb
David Triana <jdtriana@alcaldiabogota.gov.co>
2016-04-14 09:52:04 -0500

    [feature] Agrega campo boleteria resolviendo issue #1

--- templates/node--eventos.tpl.php
+++ templates/node--eventos.tpl.php
@@ -101,7 +101,8 @@
 	  </div>
 	  <div class="pure-u-1-1">
 	  	<?php print render($content['field_descripcion_evento']); ?> 
-	  	<?php print render($content['field_inscripcion_evento']); ?>
+	  	<?php print render($content['field_inscripcion_evento']); ?> 
+	  	<?php print render($content['field_boleteria_evento']); ?>
 	  	<?php print render($content['field_archivos_adjuntos_evento']); ?> 
 	  </div>
 	  <div class="videos pure-u-1-1">
